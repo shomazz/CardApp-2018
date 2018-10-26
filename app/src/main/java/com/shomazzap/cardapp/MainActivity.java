244a64926167fac899b7a30fb6d55a7f95b9f0ab
shamil <shamil10031@mail.ru>
2018-10-26 23:07:35 +0300

    Migrate to androidx

--- app/src/main/java/com/shomazzap/cardapp/MainActivity.java
+++ app/src/main/java/com/shomazzap/cardapp/MainActivity.java
@@ -4,13 +4,13 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
